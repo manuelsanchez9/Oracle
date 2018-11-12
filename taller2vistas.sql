1a7d58863fd9e07a6f228e2297d98ac429ebe02f
Manuel Sanchez <manuelsanchez128354@correo.itm.edu.co>
2018-11-12 13:12:40 -0500

    taller2vistas.sql

---
+++ taller2vistas.sql
@@ -0,0 +1,31 @@
+create or replace view medios_pago_clientes
+as
+    select cli.persona_id as cliente_id,
+           pers.nombre || ' ' || pers.apellido as nombre_cliente,
+           mdp.id as medio_pago_id, mdp.tipo_medio as tipo, mdp.datos_adicionales as detalles_medio_pago, mdp.empresarial
+    from clientes cli INNER JOIN personas pers on (cli.persona_id = pers.id)
+                      INNER JOIN medios_pago mdp on (cli.persona_id = mdp.cliente_id);
+
+--select * from medios_pago_clientes;
+
+create or replace view viajes_clientes
+as
+    select ser.fecha_hora_inicio as fecha_viaje, per_con.nombre || ' ' || per_con.apellido as nombre_conductor,
+           veh.placa as placa_vehiculo, per_cli.nombre || ' ' || per_cli.apellido as nombre_cliente,
+           sum(dp.valor_pago) over(partition by dp.servicio_id) as valor_total,
+           'FALSO' as tarifa_dinamica,
+           case ser.tipo_servicio when 'ESTANDAR' then 'UberX' else 'UberBlack' end as tipo_servicio,
+           ciu.nombre as ciudad_viaje
+    from servicios ser INNER JOIN conductores_vehiculos cv on (ser.conductor_vehiculo_id = cv.id)
+                       INNER JOIN conductores cond on (cv.conductor_id = cond.persona_id)
+                       INNER JOIN personas per_con on (cond.persona_id = per_con.id)
+                       INNER JOIN vehiculos veh on (cv.vehiculo_id = veh.id)
+                       INNER JOIN detalle_pagos dp on (ser.id = dp.servicio_id)
+                       INNER JOIN medios_pago mp on (dp.cliente_medio_pago_id = mp.id)
+                       INNER JOIN clientes cli on (mp.cliente_id = cli.persona_id)
+                       INNER JOIN personas per_cli on (cli.persona_id = per_cli.id)
+                       INNER JOIN ciudades ciu on (ser.ciudad_id = ciu.id)
+    order by per_cli.id asc, ser.fecha_hora_inicio asc;
+    
+--select * from viajes_clientes;
+                   
